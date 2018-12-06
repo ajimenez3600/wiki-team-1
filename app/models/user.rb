c800fd3e26f93cd50fb95d7350fd2936acfb4a96
Benjamin Slade <bdslade@mtu.edu>
2018-12-06 14:56:07 -0500

    Making Git Happy

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
     has_many :revisions
-    validates :name, :email, presence: true, allow_blank: false
+    validates :name, :email, :password, :password_confirmation, presence: true, allow_blank: false
     validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i }
 end
